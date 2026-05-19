733edeb122f19ade1332986729629b1283025801
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-05-19 20:34:07 +0100

    addition & multiplication

--- bigint.cpp
+++ bigint.cpp
@@ -3,20 +3,40 @@
 bigint::bigint() { nbr = "0"; }
 bigint::~bigint() {}
 bigint::bigint(const bigint &ref) { nbr = ref.nbr; }
-bigint::bigint(const uint64_t &start) { nbr = ultoa(start); }
+bigint::bigint(const uint64_t &start)
+{
+	std::string tmp;
+
+	tmp = ultoa(start);
+	for (std::size_t i = tmp.size() - 1; i; i--)
+		nbr += tmp[i];
+}
 bigint::bigint(const std::string &start)
 {
+	std::string tmp;
+
+	if (start.empty())
+		throw std::runtime_error("Error: empty string");
 	for (std::size_t i = 0; i < start.size(); i++)
 		if (!std::isdigit(start[i]))
 			throw std::runtime_error("Error: bad input");
-	nbr = start;
+	for (std::size_t i = start.size(); i; i--)
+		tmp += start[i];
+	nbr = tmp;
 }
 bigint::bigint(const char *start)
 {
-	for (std::size_t i = 0; start[i]; i++)
-		if (!std::isdigit(start[i]))
+	std::size_t i = 0;
+	std::string tmp;
+
+	if (start == NULL)
+		throw std::runtime_error("Error: NULL value");
+	while (start[i])
+		if (!std::isdigit(start[i++]))
 			throw std::runtime_error("Error: bad input");
-	nbr = start;
+	while (i)
+		tmp += start[i--];
+	nbr = tmp;
 }
 
 bigint &bigint::operator=(const bigint &ref)
@@ -43,22 +63,50 @@ bigint &bigint::operator=(const uint64_t &digit)
 	return *this;
 }
 
-bigint &bigint::operator+(const bigint &ref)
+bigint bigint::operator+(const bigint &ref) const
 {
+	bigint		result;
+	std::size_t	i = 0;
+	uint8_t		carry = 0, sum = 0;
 
+	while (i < nbr.size() || i < ref.nbr.size() || carry)
+	{
+		sum = carry;
+		if (i < nbr.size()) sum += nbr[i] - '0';
+		if (i < ref.nbr.size()) sum += ref.nbr[i] - '0';
+		result.nbr.push_back((sum % 10) + '0');
+		carry = sum / 10;
+		i++;
+	}
+	return result;
 }
 
-bigint &bigint::operator+(const uint64_t &ref)
+bigint bigint::operator+(const uint64_t &ref)
 {
+	bigint a(*this), b(ref);
+	bigint result = a + b;
 
+	return result;
 }
 
-bigint &bigint::operator+(const std::string &ref)
+bigint bigint::operator+(const std::string &ref)
 {
+	bigint a(*this), b(ref);
+	bigint result = a + b;
 
+	return result;
 }
 
-bigint &bigint::operator+(const char *ref)
+bigint bigint::operator+(const char *ref)
 {
+	bigint a(*this), b(ref);
+	bigint result = a + b;
+
+	return result;
+}
+
+bigint bigint::operator-(const bigint &ref) const
+{
+	bigint	result;
 
 }
--- bigint.hpp
+++ bigint.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <sstream>
 #include <cstddef>
 #include <cctype>
 #include <stdexcept>
@@ -17,42 +19,44 @@ class bigint
 		bigint(const uint64_t &start);
 		bigint(const std::string &start);
 		bigint(const char *start);
+		bigint	operator+(const bigint &ref);
+		bigint	operator+(const std::string &str);
+		bigint	operator+(const char *str);
+		bigint	operator+(const uint64_t &digit);
+		bigint	operator-(const bigint &ref);
+		bigint	operator-(const std::string &str);
+		bigint	operator-(const char *str);
+		bigint	operator-(const uint64_t &digit);
+		bigint	operator*(const bigint &ref);
+		bigint	operator*(const std::string &str);
+		bigint	operator*(const char *str);
+		bigint	operator*(const uint64_t &digit);
+		bigint	operator/(const bigint &ref);
+		bigint	operator/(const std::string &str);
+		bigint	operator/(const char *str);
+		bigint	operator/(const uint64_t &digit);
+		bigint	operator>>(const std::size_t &times);
+		bigint	operator<<(const std::size_t &times);
+		bool	operator==(const bigint &clone);
+		bool	operator!=(const bigint &clone);
+		bool	operator<(const bigint &clone);
+		bool	operator>(const bigint &clone);
+		bool	operator==(const std::string &clone);
+		bool	operator!=(const std::string &clone);
+		bool	operator<(const std::string &clone);
+		bool	operator>(const std::string &clone);
+		bool	operator==(const uint64_t &clone);
+		bool	operator!=(const uint64_t &clone);
+		bool	operator<(const uint64_t &clone);
+		bool	operator>(const uint64_t &clone);
+		bool	operator==(const char *clone);
+		bool	operator!=(const char *clone);
+		bool	operator<(const char *clone);
+		bool	operator>(const char *clone);
 		bigint	&operator=(const bigint &ref);
 		bigint	&operator=(const std::string &str);
 		bigint	&operator=(const char *str);
 		bigint	&operator=(const uint64_t &digit);
-		bigint	&operator+(const bigint &ref);
-		bigint	&operator+(const std::string &str);
-		bigint	&operator+(const char *str);
-		bigint	&operator+(const uint64_t &digit);
-		bigint	&operator-(const bigint &ref);
-		bigint	&operator-(const std::string &str);
-		bigint	&operator-(const char *str);
-		bigint	&operator-(const uint64_t &digit);
-		bigint	&operator*(const bigint &ref);
-		bigint	&operator*(const std::string &str);
-		bigint	&operator*(const char *str);
-		bigint	&operator*(const uint64_t &digit);
-		bigint	&operator/(const bigint &ref);
-		bigint	&operator/(const std::string &str);
-		bigint	&operator/(const char *str);
-		bigint	&operator/(const uint64_t &digit);
-		bigint	&operator>>(const std::size_t &times);
-		bigint	&operator<<(const std::size_t &times);
-		bool	&operator==(const bigint &clone);
-		bool	&operator!=(const bigint &clone);
-		bool	&operator<(const bigint &clone);
-		bool	&operator>(const bigint &clone);
-		bool	&operator==(const std::string &clone);
-		bool	&operator!=(const std::string &clone);
-		bool	&operator<(const std::string &clone);
-		bool	&operator>(const std::string &clone);
-		bool	&operator==(const uint64_t &clone);
-		bool	&operator!=(const uint64_t &clone);
-		bool	&operator<(const uint64_t &clone);
-		bool	&operator>(const uint64_t &clone);
-		bool	&operator==(const char *clone);
-		bool	&operator!=(const char *clone);
-		bool	&operator<(const char *clone);
-		bool	&operator>(const char *clone);
 };
+
+std::ostream &operator<<(std::ostream &out, const bigint &big);
